57225a716435022b231371d754d52ec86825b6c2
joxin1997 <joxinsaji30@gmail.com>
2023-04-27 00:26:02 +0530

    mongo url changed

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const PORT = 3001;
 let cors = require('cors');
 const route = require('../src/Routes')
 const mongoose = require('mongoose');
-const mongoURL = 'mongodb://localhost:27017/budget';
+// const mongoURL = 'mongodb://localhost:27017/budget';
+const mongoURL = 'mongodb+srv://joxinsaji30:cozENMB0nHcbS1RE@budgetcluster.sutxkci.mongodb.net/?retryWrites=true&w=majority';
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
